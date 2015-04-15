1f85659a212598456422a3b196beb5be8654f0c5
Alefe <alefejosefe1@hotmail.com>
2015-04-15 20:43:39 -0300

    Aula 04
    Praticas

---
+++ Aula04/Aula04 - Pratica/HoraDoSistema/src/horadosistema/HoraDoSistema.java
@@ -0,0 +1,13 @@
+package horadosistema;
+
+import java.util.Date;
+
+public class HoraDoSistema {
+
+    public static void main(String[] args) {
+        Date relogio = new Date (); // Relogio = Nome que escolhi
+        System.out.println("A hora do sistema e");
+        System.out.println(relogio.toString());
+    }
+    
+}
---
+++ Aula04/Exercicios/idiomaSistema/src/idiomasistema/IdiomaSistema.java
@@ -0,0 +1,11 @@
+package idiomasistema;
+
+public class IdiomaSistema {
+
+    public static void main(String[] args) {
+        System.getProperties(); 
+        System.out.print("O seu sistema está em ");
+        System.out.println(System.getProperty("user.language"));   
+    }
+    
+}
